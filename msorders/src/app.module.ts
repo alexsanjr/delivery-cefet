447f50e931b388251ba72d0680986474719288bd
Alexsanjr <alex.sandro.1199@gmail.com>
2025-10-18 11:21:59 -0300

    Graphql adicionado

--- msorders/src/app.module.ts
+++ msorders/src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { OrdersModule } from './orders/orders.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      playground: true,
+      sortSchema: true,
+      typePaths: ['./**/*.graphql'],
+    }),
+    OrdersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
