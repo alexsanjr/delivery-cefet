c2039c784c63a7e9512eefe05399733d3431c38e
Alexsanjr <alex.sandro.1199@gmail.com>
2025-10-18 16:23:15 -0300

    apollo server playground

--- msorders/src/app.module.ts
+++ msorders/src/app.module.ts
@@ -4,13 +4,15 @@ import { AppService } from './app.service';
 import { OrdersModule } from './orders/orders.module';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
 
 @Module({
   imports: [
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
-      playground: true,
+      playground: false,
       sortSchema: true,
+      plugins: [ApolloServerPluginLandingPageLocalDefault()],
       typePaths: ['./**/*.graphql'],
     }),
     OrdersModule,
