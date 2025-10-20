f7f1ffde4b127a06aa6e665609cba216848fac4d
burgues0 <victordpp@outlook.com>
2025-10-20 16:50:09 -0300

    fix: client interface name

--- msnotifications/src/notifications/interfaces/index.ts
+++ msnotifications/src/notifications/interfaces/index.ts
@@ -1,4 +1,4 @@
 export * from './notifications-observer.interface';
 export * from './notifications-data.interface';
 export * from './notifications-subject.interface';
-export * from './connected-client.interface';
+export * from './notifications-client.interface';
