edff0477623f70f96946c892ed2fcdc237ea11bb
burgues0 <victordpp@outlook.com>
2025-10-20 16:13:07 -0300

    edit: update index

--- msnotifications/src/notifications/interfaces/index.ts
+++ msnotifications/src/notifications/interfaces/index.ts
@@ -1,3 +1,4 @@
 export * from './notifications-observer.interface';
 export * from './notifications-data.interface';
 export * from './notifications-subject.interface';
+export * from './connected-client.interface';
