e34e3d38d4874c468ec95ff61cecc1f3e023adb1
burgues0 <victordpp@outlook.com>
2025-10-20 23:45:48 -0300

    add: module + init resolver

---
+++ msnotifications/src/graphql/notifications.resolver.ts
@@ -0,0 +1,36 @@
+import { Resolver, Query, Args } from '@nestjs/graphql';
+import { NotificationsService } from '../notifications/notifications.service';
+import { Notification } from './notifications.schema';
+
+@Resolver(() => Notification)
+export class NotificationsResolver {
+    constructor(private readonly notificationsService: NotificationsService) {}
+
+    @Query(() => [Notification], { 
+        name: 'notificationsByUser',
+        description: 'Buscar todas as notificacoes de um usuario' 
+    })
+    async getNotificationsByUser(
+        @Args('userId') userId: string
+    ): Promise<Notification[]> {
+        return await this.notificationsService.getNotificationsByUserId(userId);
+    }
+
+    @Query(() => [Notification], { 
+        name: 'notificationsByOrder',
+        description: 'Buscar todas as notificacoes de um pedido' 
+    })
+    async getNotificationsByOrder(
+        @Args('orderId') orderId: string
+    ): Promise<Notification[]> {
+        return await this.notificationsService.getNotificationsByOrderId(orderId);
+    }
+
+    @Query(() => [String], {
+        name: 'connectedClients',
+        description: 'Listar todos os clientes conectados'
+    })
+    async getConnectedClients(): Promise<string[]> {
+        return this.notificationsService.getConnectedClients();
+    }
+}
