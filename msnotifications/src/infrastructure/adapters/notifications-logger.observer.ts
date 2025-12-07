294a532ec198817562427a8c6d4f75129df08fd5
burgues0 <victordpp@outlook.com>
2025-12-07 20:11:51 -0300

    edit: adapters

---
+++ msnotifications/src/infrastructure/adapters/notifications-logger.observer.ts
@@ -0,0 +1,17 @@
+import { Injectable, Logger } from '@nestjs/common';
+import type { NotificationObserverPort } from '../../domain/ports/notification-observer.port';
+import type { NotificationData } from '../../domain/interfaces/notification-data.interface';
+
+@Injectable()
+export class NotificationLoggerObserver implements NotificationObserverPort {
+    private readonly logger = new Logger(NotificationLoggerObserver.name);
+    
+    async update(notification: NotificationData): Promise<void> {
+        this.logger.log(
+            `Notificação - Pedido: ${notification.orderId}, ` +
+            `Usuário: ${notification.userId}, ` +
+            `Status: ${notification.status}, ` +
+            `Origem: ${notification.serviceOrigin}`
+        );
+    }
+}
