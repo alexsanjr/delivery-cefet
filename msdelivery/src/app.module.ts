76265e496e20e57d4e3a6ef5c415ee8df968f292
Joao Abreu <joaopedro.g.abreu@outlook.com>
2025-12-09 01:59:14 -0300

    feat(msdelivery): atualizar módulos gRPC e remover GraphQL

--- msdelivery/src/app.module.ts
+++ msdelivery/src/app.module.ts
@@ -1,30 +1,15 @@
 import { Module } from '@nestjs/common';
-import { GraphQLModule } from '@nestjs/graphql';
-import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
-import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
-import { join } from 'path';
 import { PrismaModule } from './prisma/prisma.module';
 import { DeliveryPersonsModule } from './delivery-persons/delivery-persons.module';
 import { DeliveriesModule } from './deliveries/deliveries.module';
 import { GrpcModule } from './grpc/grpc.module';
-import { DeliveryGrpcModule } from './grpc/delivery-grpc.module';
 
 @Module({
   imports: [
-    GraphQLModule.forRoot<ApolloDriverConfig>({
-      driver: ApolloDriver,
-      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
-      sortSchema: true,
-      introspection: true,
-      plugins: [ApolloServerPluginLandingPageLocalDefault()],
-      context: ({ req }) => ({ req }),
-    }),
-    
     PrismaModule,
     GrpcModule,
     DeliveryPersonsModule,
     DeliveriesModule,
-    DeliveryGrpcModule,
   ],
 })
 export class AppModule {}
