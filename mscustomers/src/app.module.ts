38608c5db97a6b5721604b509c39b2d7545a83be
levyath <levyarthur2@gmail.com>
2025-12-08 21:27:52 -0300

    Configura módulos para integração com RabbitMQ

--- mscustomers/src/app.module.ts
+++ mscustomers/src/app.module.ts
@@ -1,13 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './infrastructure/prisma/prisma.module';
+import { RabbitMQModule } from './infrastructure/messaging/rabbitmq.module';
 import { GraphqlModule } from './presentation/graphql/graphql.module';
 import { GrpcModule } from './presentation/grpc.module';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
 
-// Módulo raiz: integra GraphQL, gRPC e configurações gerais
+// Módulo raiz: integra GraphQL, gRPC, RabbitMQ e configurações gerais
 @Module({
   imports: [
     ConfigModule.forRoot({
@@ -23,6 +24,7 @@ import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin
       plugins: [ApolloServerPluginLandingPageLocalDefault()],
     }),
     PrismaModule,
+    RabbitMQModule,
     GraphqlModule,
     GrpcModule,
   ],
