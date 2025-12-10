2f91adc8eb58ebc9cdda08eeb80a01fdd6d11e08
burgues0 <victordpp@outlook.com>
2025-12-09 21:22:20 -0300

    edit: 4 spaces

--- mstracking/src/app.module.ts
+++ mstracking/src/app.module.ts
@@ -3,32 +3,34 @@ import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { join } from 'path';
-import { TrackingModule } from './tracking/tracking.module';
-import { GrpcModule } from './grpc/grpc.module';
+import { TrackingInfrastructureModule } from './infrastructure/infrastructure.module';
+import { TrackingApplicationModule } from './application/application.module';
+import { GrpcPresentationModule } from './presentation/grpc/grpc.module';
+import { GraphQLPresentationModule } from './presentation/graphql/graphql.module';
 
 @Module({
-  imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      url: process.env.DATABASE_URL,
-      entities: [__dirname + '/**/*.entity{.ts,.js}'],
-      synchronize: true,
-    }),
-    GraphQLModule.forRoot<ApolloDriverConfig>({
-      driver: ApolloDriver,
-      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
-      playground: true,
-      introspection: true,
-      csrfPrevention: false,
-      installSubscriptionHandlers: true,
-      subscriptions: {
-        'graphql-ws': true,
-      },
-    }),
-    TrackingModule,
-    GrpcModule,
-  ],
-  controllers: [],
-  providers: [],
+    imports: [
+        TypeOrmModule.forRoot({
+            type: 'postgres',
+            url: process.env.DATABASE_URL,
+            entities: [__dirname + '/infrastructure/persistence/*.orm{.ts,.js}'],
+            synchronize: true,
+        }),
+        GraphQLModule.forRoot<ApolloDriverConfig>({
+            driver: ApolloDriver,
+            autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+            playground: true,
+            introspection: true,
+            csrfPrevention: false,
+            installSubscriptionHandlers: true,
+            subscriptions: {
+                'graphql-ws': true,
+            },
+        }),
+        TrackingInfrastructureModule,
+        TrackingApplicationModule,
+        GrpcPresentationModule,
+        GraphQLPresentationModule,
+    ],
 })
 export class AppModule {}
