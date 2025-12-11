f83e8e67bc37f09b27ec1fe2a0ac2a3126e2a475
burgues0 <victordpp@outlook.com>
2025-12-11 15:11:33 -0300

    fix: container

--- bff-delivery/src/dataloaders/dataloaders.module.ts
+++ bff-delivery/src/dataloaders/dataloaders.module.ts
@@ -1,16 +1,33 @@
 import { Module, Scope } from '@nestjs/common';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { join } from 'path';
 import { DeliveryPersonLoader } from './delivery-person.loader';
 import { DeliveryServiceImpl } from '../delivery/delivery.service';
 
 @Module({
+  imports: [
+    ClientsModule.register([{
+      name: 'DELIVERY_PACKAGE',
+      transport: Transport.GRPC,
+      options: {
+        package: ['deliveryperson', 'delivery'],
+        protoPath: [
+          join(__dirname, '../../proto/delivery-person.proto'),
+          join(__dirname, '../../proto/delivery.proto'),
+        ],
+        url: process.env.DELIVERY_GRPC_URL || 'msdelivery:50056',
+      },
+    }]),
+  ],
   providers: [
+    DeliveryServiceImpl,
     {
       provide: DeliveryPersonLoader,
       useFactory: (deliveryService: DeliveryServiceImpl) => {
         return new DeliveryPersonLoader(deliveryService);
       },
-      inject: ['DeliveryService'],
-      scope: Scope.REQUEST, // Nova instância por request - evita cache entre usuários
+      inject: [DeliveryServiceImpl],
+      scope: Scope.REQUEST,
     },
   ],
   exports: [DeliveryPersonLoader],
