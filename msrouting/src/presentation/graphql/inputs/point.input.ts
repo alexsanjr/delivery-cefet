decd742d11127c4e4b480610ae2149a5c948ec2c
levyath <levyarthur2@gmail.com>
2025-12-13 18:17:59 -0300

    ajuste em point.input.ts em msrouting

--- msrouting/src/presentation/graphql/inputs/point.input.ts
+++ msrouting/src/presentation/graphql/inputs/point.input.ts
@@ -1,10 +1,17 @@
 import { InputType, Field, Float } from '@nestjs/graphql';
+import { IsNumber, Min, Max } from 'class-validator';
 
 @InputType()
 export class PointInput {
   @Field(() => Float, { description: 'Latitude do ponto geográfico' })
+  @IsNumber()
+  @Min(-90)
+  @Max(90)
   latitude: number;
 
   @Field(() => Float, { description: 'Longitude do ponto geográfico' })
+  @IsNumber()
+  @Min(-180)
+  @Max(180)
   longitude: number;
 }
